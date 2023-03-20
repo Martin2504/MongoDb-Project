f7614fc14369509166988cab55367fe6d9ae2a8f
HMalik <hmalik-@hotmail.com>
2023-03-20 15:43:21 +0000

    Changes made on usercontroller

--- src/main/java/com/sparta/spartamongodbfinalproject/controllers/rest_controllers/UsersRestController.java
+++ src/main/java/com/sparta/spartamongodbfinalproject/controllers/rest_controllers/UsersRestController.java
@@ -1,7 +1,22 @@
 package com.sparta.spartamongodbfinalproject.controllers.rest_controllers;
 
+import com.sparta.spartamongodbfinalproject.model.repositories.UsersRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsersRestController {
+
+    private final UsersRepositories usersRepositories;
+
+    @Autowired
+    public UsersRestController(UsersRepositories usersRepositories) {
+        this.usersRepositories = usersRepositories;
+    }
+
+    @PostMapping()
+
+
+
 }
