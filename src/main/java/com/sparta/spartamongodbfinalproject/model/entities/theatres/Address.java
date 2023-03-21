5f2267eee45b9d5e1e75ca48c2694841cb915acb
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-03-21 17:01:07 +0000

    Theatres Web Controller + Welcome Web Controller.

--- src/main/java/com/sparta/spartamongodbfinalproject/model/entities/theatres/Address.java
+++ src/main/java/com/sparta/spartamongodbfinalproject/model/entities/theatres/Address.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 @Accessors(chain = true)
-@AllArgsConstructor
 @Setter
 @Getter
 public class Address {
@@ -16,9 +15,22 @@ public class Address {
 //    zipcode "93906"
     private String street1;
 
+    private String street2;
+
     private String city;
 
     private String state;
 
     private String zipcode;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street1='" + street1 + '\'' +
+                ", street2='" + street2 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                '}';
+    }
 }
