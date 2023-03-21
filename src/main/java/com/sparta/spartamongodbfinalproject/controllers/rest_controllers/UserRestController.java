fe8925834b802d7e1b6a981cca1cb02f36c4423d
HMalik <hmalik-@hotmail.com>
2023-03-21 14:47:02 +0000

    Added Object mapper to getMapping in controller

--- src/main/java/com/sparta/spartamongodbfinalproject/controllers/rest_controllers/UserRestController.java
+++ src/main/java/com/sparta/spartamongodbfinalproject/controllers/rest_controllers/UserRestController.java
@@ -1,34 +1,57 @@
 package com.sparta.spartamongodbfinalproject.controllers.rest_controllers;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.spartamongodbfinalproject.model.entities.User;
 import com.sparta.spartamongodbfinalproject.model.repositories.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class UserRestController {
 
     private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
 
 
     @Autowired
-    public UserRestController(UserRepository userRepository) {
+    public UserRestController(UserRepository userRepository, ObjectMapper objectMapper) {
         this.userRepository = userRepository;
+        this.objectMapper = objectMapper;
     }
 
 
+
+//    @GetMapping("/api/user")
+//    public ResponseEntity<List<User>> getUsersByName(@RequestParam("name") String name) {
+//        List<User> users = userRepository.findByName(name);
+//        if (!users.isEmpty()) {
+//            return ResponseEntity.ok(users);
+//        } else {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(users);
+//        }
+//    }
+
     // Read user details when given a name
     @GetMapping("/api/user")
-    public ResponseEntity<List<User>> getUsersByName(@RequestParam("name") String name) {
+    public ResponseEntity<String> getUsersByName(@RequestParam("name") String name) {
         List<User> users = userRepository.findByName(name);
-        if (!users.isEmpty()) {
-            return ResponseEntity.ok(users);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(users);
+        try {
+            String json = objectMapper.writeValueAsString(users);
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+            if (!users.isEmpty()) {
+                return ResponseEntity.ok().headers(httpHeaders).body(json);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(httpHeaders).body(json);
+            }
+        } catch (JsonProcessingException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
