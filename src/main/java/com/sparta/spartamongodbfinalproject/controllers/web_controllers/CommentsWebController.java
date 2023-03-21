c26b424e1de5d6eb9af14234a8e78bba6bf850ce
Liam <liambramah@gmail.com>
2023-03-21 10:41:51 +0000

    commited webcontroller

--- src/main/java/com/sparta/spartamongodbfinalproject/controllers/web_controllers/CommentsWebController.java
+++ src/main/java/com/sparta/spartamongodbfinalproject/controllers/web_controllers/CommentsWebController.java
@@ -1,27 +1,32 @@
 package com.sparta.spartamongodbfinalproject.controllers.web_controllers;
 
+import com.sparta.spartamongodbfinalproject.SpartaMongoDbFinalProjectApplication;
 import com.sparta.spartamongodbfinalproject.controllers.StringFormatter;
 import com.sparta.spartamongodbfinalproject.model.entities.Comments;
 import com.sparta.spartamongodbfinalproject.model.repositories.CommentsRepository;
+import com.sparta.spartamongodbfinalproject.model.repositories.MoviesRepository;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class CommentsWebController {
 
     private final CommentsRepository commentsRepository;
+    private final MoviesRepository moviesRepository;
 
-    public CommentsWebController(CommentsRepository commentsRepository) {
+    public CommentsWebController(CommentsRepository commentsRepository,
+                                 MoviesRepository moviesRepository) {
         this.commentsRepository = commentsRepository;
+        this.moviesRepository = moviesRepository;
     }
 
     @GetMapping("/comments")
     public String commentsHome(){
-        return "comments-page";
+        return "mainPages/comments-page";
     }
 
     @GetMapping("/comments/search")
@@ -31,18 +36,35 @@ public class CommentsWebController {
 
     @GetMapping("/comments/results")
     public String getCommentSearchResults(Model model, @RequestParam String name){
-        model.addAttribute("comments", commentsRepository.findCommentsByNameEquals(StringFormatter.capitalizeWord(name)));
+        model.addAttribute("comments", commentsRepository.findCommentsByNameEquals(name));
         return "comments/comment-search-results";
     }
 
-    @GetMapping("comments/create")
+    @GetMapping("/comments/create")
     public String getCommentCreateForm(){
         return "comments/comment-create-form";
     }
 
     @PostMapping("/createcomment")
-    public String createComment(@ModelAttribute Comments comment){
+    public String createComment(@ModelAttribute("commentToCreate") Comments comment){
+        comment.setDate(LocalDateTime.now());
+        comment.setMovie_id(moviesRepository.findMoviesByTitleEquals());
+        SpartaMongoDbFinalProjectApplication.logger.info(comment.toString());
         return "comments/comment-create-success";
     }
+
+    @GetMapping("/comments/edit/{commentId}")
+    public String getCommentEditForm(@PathVariable ObjectId commentId, Model model){
+        Comments comment = commentsRepository.findById(commentId).orElse(null);
+        model.addAttribute("commentToEdit", comment);
+        return "comments/comment-edit-form";
+    }
+
+    @PostMapping("editcomment")
+    public String editComment(@ModelAttribute("commentToEdit") Comments comment){
+        SpartaMongoDbFinalProjectApplication.logger.info(comment.toString());
+        return "comments/comment-edit-success";
+    }
+
 
 }
--- src/main/java/com/sparta/spartamongodbfinalproject/model/repositories/MoviesRepository.java
+++ src/main/java/com/sparta/spartamongodbfinalproject/model/repositories/MoviesRepository.java
@@ -5,4 +5,5 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface MoviesRepository extends MongoRepository<Movies, ObjectId> {
+    Movies findMoviesByTitleEquals(String title);
 }
