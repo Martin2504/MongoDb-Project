4e97de17fe12ddf20a26870398c60cccee067809
Miguel <miguel.camilleri1@gmail.com>
2023-03-22 14:53:47 +0000

    added methods to repo

--- src/main/java/com/sparta/spartamongodbfinalproject/controllers/rest_controllers/ScheduleRestController.java
+++ src/main/java/com/sparta/spartamongodbfinalproject/controllers/rest_controllers/ScheduleRestController.java
@@ -2,16 +2,21 @@ package com.sparta.spartamongodbfinalproject.controllers.rest_controllers;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.spartamongodbfinalproject.model.entities.Movie;
 import com.sparta.spartamongodbfinalproject.model.entities.Schedule;
 import com.sparta.spartamongodbfinalproject.model.entities.Theatre;
 import com.sparta.spartamongodbfinalproject.model.repositories.MovieRepository;
 import com.sparta.spartamongodbfinalproject.model.repositories.ScheduleRepository;
 import com.sparta.spartamongodbfinalproject.model.repositories.TheatreRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -35,10 +40,25 @@ public class ScheduleRestController {
         return scheduleRepository.findAll();
     }
 
-    @GetMapping(value = "/api/schedules/{id}")
-    public Schedule getAllSchedulesByObjectId(@PathVariable String id) {
-        return scheduleRepository.findScheduleById(id);
+    @GetMapping(value = "/api/schedules/object/{objectId}")
+    public Schedule getAllSchedulesByObjectId(@PathVariable String objectId) {
+        return scheduleRepository.findScheduleById(objectId);
     }
+
+    @GetMapping(value = "/api/schedules/theatre/{theatreId}")
+    public Schedule getAllSchedulesByTheatreId(@PathVariable ObjectId theatreId) {
+        return scheduleRepository.findScheduleByTheatreId(theatreId);
+    }
+
+    @GetMapping(value = "/api/schedules/movie/{title}")
+    public List<Schedule> getAllSchedulesByMovieId(@PathVariable String title) {
+        Movie movie = movieRepository.findMovieByTitleEquals(title).orElse(null);
+        return scheduleRepository.findScheduleByMovieId(movie.getId());
+    }
+
+
+
+
 
 
 
--- src/main/java/com/sparta/spartamongodbfinalproject/model/repositories/ScheduleRepository.java
+++ src/main/java/com/sparta/spartamongodbfinalproject/model/repositories/ScheduleRepository.java
@@ -2,14 +2,30 @@ package com.sparta.spartamongodbfinalproject.model.repositories;
 
 import com.sparta.spartamongodbfinalproject.model.entities.Schedule;
 import com.sparta.spartamongodbfinalproject.model.entities.Theatre;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ScheduleRepository extends MongoRepository<Schedule, String> {
 
 
         @Query("{ '_id' : ?0 }")
         Schedule findScheduleById(String id);
+
+        @Query("{ 'theater_id' : ?0 }")
+        Schedule findScheduleByTheatreId(ObjectId objectId);
+
+        @Query("{ 'movie_id' : ?0 }")
+        List<Schedule> findScheduleByMovieId(ObjectId movieId);
+
+        @Query("{ 'startTime' : ?0 }")
+        List<Schedule> findScheduleByStartTime(LocalDateTime startTime);
+
+        Schedule findScheduleByMovie_Id(String id);
+
 
 
 
