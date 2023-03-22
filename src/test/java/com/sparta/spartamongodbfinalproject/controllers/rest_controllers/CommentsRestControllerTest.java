2c99de0bfc9fb0d032e6a211d416f72110f19214
Jane Doe <faris.a@hotmail.co.uk>
2023-03-22 13:51:27 +0000

    x

--- src/test/java/com/sparta/spartamongodbfinalproject/controllers/rest_controllers/CommentsRestControllerTest.java
+++ src/test/java/com/sparta/spartamongodbfinalproject/controllers/rest_controllers/CommentsRestControllerTest.java
@@ -1,6 +1,9 @@
 package com.sparta.spartamongodbfinalproject.controllers.rest_controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +11,13 @@ class CommentsRestControllerTest {
 
     @Test
     void createComment() {
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.get("/api/movie/6419a1e9821423636388b9d"))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -17,6 +27,7 @@ class CommentsRestControllerTest {
     @Test
     void updateComment() {
     }
+
 
     @Test
     void deleteComment() {
